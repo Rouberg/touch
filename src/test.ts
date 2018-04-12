97a94f0e3cee4872d5b5225e4842fd9fdd124874
rou <rou@Tiny.local>
2018-04-12 13:52:59 +0800

    update

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,13 @@
 enum DataTypes {
-  Number,
-  String,
-  Array,
-  Object,
-  Function,
-  Date,
-  Symbol,
-  Null,
-  Undefined
+  Number = 'number',
+  String = '\'\'',
+  Array = '[]',
+  Object = '{}',
+  Function = 'function(){}',
+  Date = 'new Date()',
+  Symbol = 'new Symbol(0)',
+  Null = 'null',
+  Undefined = ''
 }
 
 export default DataTypes
