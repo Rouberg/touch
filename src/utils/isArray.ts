e2d82cf23a1562b1704df4c1bfcce53a6ac8a38a
Rou <Rou@Tiny.local>
2018-06-01 13:45:35 +0800

    update

--- src/utils/isArray.ts
+++ src/utils/isArray.ts
@@ -1,8 +1,8 @@
-import is from './is'
+import is from './is';
 
 /**
  * 判断一个数据是否是一个数组
  * 对一个数组使用typeof运算会返回object
  */
-const isArray: (args: any) => boolean = Array.isArray || is('array')
-export default isArray
+const isArray: (args: any) => boolean = Array.isArray || is('array');
+export default isArray;
